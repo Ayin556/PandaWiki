738059536dc620d0885c3628419a99afe24f8acf
廖家银 <liaojiayin@dslyy.com>
2026-05-24 11:54:29 +0800

    py版本1.1支持go和pyhton后端服务。

---
+++ web/admin/.env.go
@@ -0,0 +1,3 @@
+TARGET=http://localhost:8080
+STATIC_FILE_TARGET=http://localhost:8080
+SHARE_TARGET=http://localhost:8080
---
+++ web/app/.env.go
@@ -0,0 +1,3 @@
+TARGET=http://localhost:8080
+STATIC_FILE_TARGET=http://localhost:8080
+DEV_KB_ID=43d4ba40-bc1a-4151-9ad0-cf0e6604cfa3
